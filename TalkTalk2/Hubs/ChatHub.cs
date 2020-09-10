3f4bc94f907464aedb8be518062ef9a5abe1ce34
ayat hosam <ayathosam20@gmail.com>
2020-09-10 22:49:32 +0200

    first commit

---
+++ TalkTalk2/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalkTalk2.Models;
+
+namespace TalkTalk2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessages(Message message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
---
+++ TalkTalk2/Models/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalkTalk2.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public DateTime When { get; set; }
+
+        public string UserID { get; set; }
+
+        public virtual AppUser Sender { get; set; }
+
+        public Message()
+        {
+            When = DateTime.Now;
+        }
+    }
+}
